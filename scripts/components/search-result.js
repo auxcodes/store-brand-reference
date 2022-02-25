532f8c5e237852915432f5753b3eca655879b724
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2022-02-26 10:25:00 +1100

    Remove Edit button

--- scripts/components/search-result.js
+++ scripts/components/search-result.js
@@ -3,7 +3,6 @@ class SearchResult extends HTMLElement {
         this.innerHTML = `
             <span class='brand-name'>${result.storeName}</span> 
             <a class='brand-url' href='${result.storeURL}' target='_blank'>${result.storeURL}</a>
-            <button id='editStoreBtn' class='edit-btn' title='Edit Store' onclick='onEditStore()'></button> 
             <span class='brand-list'><b>Brands: </b>${result.brands}</span>
         `;
     }
