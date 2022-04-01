1026ca134a268722e8122914669dd479fd54c5c2
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2022-04-01 14:43:52 +1100

    Initial test of new server function

---
+++ netlify_functions/fbauth.js
@@ -0,0 +1,42 @@
+const admin = require('firebase-admin')
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://store-search-d8833-default-rtdb.europe-west1.firebasedatabase.app/"
+});
+
+const db = admin.firestore();
+
+exports.handler = async (event, context, callback) => {
+    const body = JSON.parse(event.body);
+    const errors = [];
+    const email = body['auth'].email.toLowerCase();
+
+    if (email.includes("@99bikes.com.au")) {
+        callback(null, {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({ msg: JSON.stringify(serviceAccount), error: errors })
+        });
+    }
+    else {
+        errors.push(new Error("Email was not valid"));
+        callback(null, {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({ msg: "Validation Failed", error: errors })
+        });
+    }
+}
