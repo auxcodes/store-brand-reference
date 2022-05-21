97b0888b891804ee15fb078463cc4c64f201b1e3
Phill <dev@aux.codes>
2022-05-21 12:19:38 +1000

    Convert Notifications to Components

---
+++ src/app/components/notification-modal.js
@@ -0,0 +1,10 @@
+class NotificationModal extends HTMLElement {
+    set notification(content) {
+        this.innerHTML = `
+            <span onclick="onCloseNotification('${content.id}')" class="notif-close" title="Close Modal">&times;</span>
+            <span class="notif-text">${content.text}</span>    
+        `;
+    }
+}
+
+customElements.define('notification-modal', NotificationModal);
--- src/app/notifications.js
+++ src/app/notifications.js
@@ -1,3 +1,5 @@
+import { } from "./components/notification-modal.js";
+
 let localStorageService = null;
 let notificationHistory = {};
 const storageKey = 'notifications';
@@ -5,39 +7,31 @@ const storageKey = 'notifications';
 const notificationObjects = [
     {
         id: "notif-01",
-        content: `
-        <span class="notif-text">This is currently a proof of concept; shop information is currently incomplete as it is being added manually. </span><br />
-        <span class="notif-text">Search results will not provide all available options. </span><br />
-        <span class="notif-text">Future updates will include: update/edit shop information, add new shop and login only access.</span>
+        text: `
+        This is currently a proof of concept; shop information is currently incomplete as it is being added manually.<br />
+        Search results will not provide all available options.<br />
+        Future updates will include: update/edit shop information, add new shop and login only access.
         `,
         show: true
     },
     {
         id: "notif-2501",
-        content: `
-        <span class="notif-text">Update 25/01: Added link to PSI's new website. </span>    
-        `,
+        text: "Update 25/01: Added link to PSI's new website.",
         show: true
     },
     {
         id: "notif-2402",
-        content: `
-        <span class="notif-text">Update 24/02: Added POC, Updated LinkSports, Jetblack, Family Distribution </span>    
-        `,
+        text: "Update 24/02: Added POC, Updated LinkSports, Jetblack, Family Distribution",
         show: true
     },
     {
         id: "notif-2602",
-        content: `
-        <span class="notif-text">Update 26/02: Updated Advanced Traders, Groupe Sportif </span>    
-        `,
+        text: "Update 26/02: Updated Advanced Traders, Groupe Sportif",
         show: true
     },
     {
         id: "notif-0203",
-        content: `
-        <span class="notif-text">Update 02/03: Added Shepards Scott B2B, Updated Garmin </span>    
-        `,
+        text: "Update 02/03: Added Shepards Scott B2B, Updated Garmin",
         show: true
     }
 ];
@@ -53,11 +47,12 @@ function generateNotifications() {
     checkLocalStorage().then(() => {
         notificationObjects.forEach(item => {
             if (item.show) {
-                const div = document.createElement('div');
-                div.classList.add('notification-bar');
-                div.id = item.id;
-                div.innerHTML = `<span onclick="onCloseNotification('${item.id}')" class="notif-close" title="Close Modal">&times;</span>` + item.content;
-                notificationsContainer.append(div);
+                const modal = document.createElement('notification-modal');
+                modal.id = item.id;
+                modal.classList.add('notification-bar');
+                modal.notification = item;
+
+                notificationsContainer.append(modal);
             }
         });
     });
