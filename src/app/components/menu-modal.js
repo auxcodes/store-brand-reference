f2994ba3e94fd325c70927825c5c84781379f480
Phill <dev@aux.codes>
2022-05-27 21:13:28 +1000

    Convert Menu to Buttons

--- src/app/components/menu-modal.js
+++ src/app/components/menu-modal.js
@@ -3,14 +3,14 @@ class MenuModal extends HTMLElement {
         this.innerHTML = `
             <nav id="site-nav" class="site-nav">
                 <ul>
-                    <li id="add-shop-link"><a ><i class="fa fa-plus site-nav--icon"></i>Add Shop</a></li>
-                    <li id="contact-link"><a ><i class="fa fa-envelope site-nav--icon"></i>Contact</a></li>
-                    <li id="logout-link"><a ><i class="fa fa-sign-out site-nav--icon"></i>Logout</a></li>
-                    <li id="login-link"><a ><i class="fa fa-sign-in site-nav--icon"></i>Login</a></li>
+                    <li id="add-shop-link"><button title="Add new shop"><i class="fa fa-plus site-nav--icon"></i>Add Shop</button></li>
+                    <li id="contact-link"><button title="Contact Us"><i class="fa fa-envelope site-nav--icon"></i>Contact</button></li>
+                    <li id="logout-link"><button title="Logout"><i class="fa fa-sign-out site-nav--icon"></i>Logout</button></li>
+                    <li id="login-link"><button title="Login"><i class="fa fa-sign-in site-nav--icon"></i>Login</button></li>
                 </ul>
             </nav>
             <div id="menu-toggle" class="menu-toggle">
-                <div class="hamburger"></div>
+                <button class="hamburger" title="Main menu"></button>
             </div>
             <button id="notif-toggle" class="notification-toggle" title="Notifications"><i id="notif-icon" class="fa fa-bell notif-toggle--icon"></i></button>
         `;
