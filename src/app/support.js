876b4ced4e2e37461df1c3144cc15e6cc97c3e06
Phill <dev@aux.codes>
2022-06-17 19:56:36 +1000

    Insert GA Script

--- src/app/support.js
+++ src/app/support.js
@@ -2,11 +2,15 @@ import { debugOn, getGaId } from "./environment.js";
 
 export function insertGaScript() {
     const gaID = getGaId();
-    const gaTag = document.getElementById('ga-tag');
+    const bodyEl = document.getElementsByTagName('body')[0];
+
+    const gaTag = document.createElement('script');
     gaTag.innerHTML = `window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
     gtag('config', '${gaID}');`;
+    console.log('body element: ', bodyEl);
+    bodyEl.append(gaTag);
 }
 
 export function onStoreClick(storeName) {
@@ -14,37 +18,41 @@ export function onStoreClick(storeName) {
 }
 
 export function productSearchEvent(searchTerm) {
-    if (debugOn()) { console.log('GA - Product Search Event: ', searchTerm); }
-    gtag('event', 'search_products', {
+    const eventInfo = {
         'event_category': 'search',
         'event_label': searchTerm,
         'debug_mode': debugOn()
-    });
+    };
+    gtag('event', 'search_products', eventInfo);
+    if (debugOn()) { console.log('GA - Product Search Event: ', eventInfo); }
 }
 
 export function brandSearchEvent(searchTerm) {
-    if (debugOn()) { console.log('GA - Brand Search Event: ', searchTerm); }
-    gtag('event', 'search_brands', {
+    const eventInfo = {
         'event_category': 'search',
         'event_label': searchTerm,
         'debug_mode': debugOn()
-    });
+    };
+    gtag('event', 'search_brands', eventInfo);
+    if (debugOn()) { console.log('GA - Brand Search Event: ', searchTerm); }
 }
 
 export function noResultEvent(searchTerm) {
-    if (debugOn()) { console.log('GA - Failed Search Event: ', searchTerm); }
-    gtag('event', 'failed_search', {
+    const eventInfo = {
         'event_category': 'search',
         'event_label': searchTerm,
         'debug_mode': debugOn()
-    });
+    };
+    gtag('event', 'failed_search', eventInfo);
+    if (debugOn()) { console.log('GA - Failed Search Event: ', searchTerm); }
 }
 
 function storeClickEvent(storeName) {
-    if (debugOn()) { console.log('GA - Store Click Event: ', storeName); }
-    gtag('event', 'store_clicked', {
+    const eventInfo = {
         'event_category': 'click',
         'event_label': storeName,
         'debug_mode': debugOn()
-    });
+    };
+    gtag('event', 'store_clicked', eventInfo);
+    if (debugOn()) { console.log('GA - Store Click Event: ', storeName); }
 }
