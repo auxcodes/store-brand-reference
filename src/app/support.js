c3aa968ba7f7070f28447d5503190a4deb6b5362
Phill <dev@aux.codes>
2022-06-17 14:04:35 +1000

    Add debug to GA events

--- src/app/support.js
+++ src/app/support.js
@@ -1,4 +1,4 @@
-import { getGaId } from "./environment.js";
+import { debugOn, getGaId } from "./environment.js";
 
 export function insertGaScript() {
     const gaID = getGaId();
@@ -14,30 +14,37 @@ export function onStoreClick(storeName) {
 }
 
 export function productSearchEvent(searchTerm) {
+    if (debugOn()) { console.log('GA - Product Search Event: ', searchTerm); }
     gtag('event', 'search_products', {
         'event_category': 'search',
-        'event_label': searchTerm
+        'event_label': searchTerm,
+        'debug_mode': debugOn()
     });
 }
 
 export function brandSearchEvent(searchTerm) {
+    if (debugOn()) { console.log('GA - Brand Search Event: ', searchTerm); }
     gtag('event', 'search_brands', {
         'event_category': 'search',
-        'event_label': searchTerm
+        'event_label': searchTerm,
+        'debug_mode': debugOn()
     });
 }
 
 export function noResultEvent(searchTerm) {
+    if (debugOn()) { console.log('GA - Failed Search Event: ', searchTerm); }
     gtag('event', 'failed_search', {
         'event_category': 'search',
-        'event_label': searchTerm
+        'event_label': searchTerm,
+        'debug_mode': debugOn()
     });
 }
 
 function storeClickEvent(storeName) {
+    if (debugOn()) { console.log('GA - Store Click Event: ', storeName); }
     gtag('event', 'store_clicked', {
         'event_category': 'click',
-        'event_label': storeName
-        //, 'debug_mode': true
+        'event_label': storeName,
+        'debug_mode': debugOn()
     });
 }
