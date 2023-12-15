1ea40e338a5cab59d044fb5d5b5b5826a9b458b7
Auxcodes <dev@aux.codes>
2023-12-15 17:27:37 +1100

    Style landing page and add a couple of buttons

--- src/app/components/search-form-landing.js
+++ src/app/components/search-form-landing.js
@@ -3,9 +3,20 @@ class SearchFormLanding extends HTMLElement {
     this.innerHTML = `
             <form id="form-search-landing" class="search-form">
                 <div class="search-input-row">
-                    <label class="search-label" for="searchInput">Store Search:</label>
-                    <input class="search-input" id="searchInput" type="search"
+                    <div class="label-platform">
+                        <label class="search-label" for="searchInput">Store Search:</label>
+                    </div>
+                    <div class="input-container"> 
+                        <i class="fa-solid fa-magnifying-glass .search-icon" style="position: absolute;top: 3.5em;left: 1em;"></i>
+                        <input class="search-input" id="searchInput" type="search"
                         placeholder="Search for brands or products..." />
+                    </div>
+                </div>
+                <div class="search-btn-container">
+                    <button class="search-btn search-all-btn" id="searchBtn" type="button"
+                        title="Search all stores">Store Search</button>
+                    <button class="search-btn browse-btn" id="browseBtn" type="button"
+                        title="Browse all stores">Browse Stores</button>
                 </div>
             </form>
 `;
