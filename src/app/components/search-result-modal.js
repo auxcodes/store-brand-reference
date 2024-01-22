f8228fe92e71f99a62b0e9c45f552de13962c045
Auxcodes <dev@aux.codes>
2024-01-22 16:12:11 +1100

    Add some juice to style transitions.

--- src/app/components/search-result-modal.js
+++ src/app/components/search-result-modal.js
@@ -1,13 +1,19 @@
 class SearchResult extends HTMLElement {
   set result(result) {
     this.innerHTML = `
+        <div class='result-header'>
             <span class='brand-name'>${result.shopName}</span> 
             <a id='shopUrl' class='brand-url' href='${result.shopURL}' target='_blank'>${result.shopURL}</a>
-            <button id='viewStoreBtn' class='edit-btn' title='View Shop Information'><i class="fa-solid fa-info view-btn"></i></button> 
-            <span class='brand-list'><b>Brands: </b>${result.brands}</span>
-            <span class='product-list'><b>Products: </b>${result.parts}</span>
-            <span class='warranty-list'><b>Warranty: </b>${result.shopWarranty}</span>
-            <span class='shop-list'><b>Phone: </b>${result.shopPhone} <b>Email: </b> ${result.shopEmail} <b>Address: </b> ${result.shopAddress}</span>
+            <button id='viewStoreBtn' class='edit-btn' title='View Shop Information'><i class="fa-solid fa-info view-btn"></i></button>
+        </div>
+        <div class='result-body'>
+            <div>
+              <span class='brand-list'><b>Brands: </b>${result.brands}</span>
+              <span class='product-list'><b>Products: </b>${result.parts}</span>
+              <span class='warranty-list'><b>Warranty: </b>${result.shopWarranty}</span>
+              <span class='shop-list'><b>Phone: </b>${result.shopPhone} <b>Email: </b> ${result.shopEmail} <b>Address: </b> ${result.shopAddress}</span>
+            </div> 
+        </div>
             <span id='shopId' class='shop-id'>${result.shopId}</span>
         `;
   }
--- src/app/ui.js
+++ src/app/ui.js
@@ -22,8 +22,8 @@ function colourSwap(searchType, activateColour) {
   activeColour.style.setProperty("--show-products", searchType === "parts" ? "block" : "none");
   activeColour.style.setProperty("--show-warranty", searchType === "warranty" ? "block" : "none");
   activeColour.style.setProperty("--show-shop", searchType === "shops" ? "block" : "none");
-  searchBrandBtn.className = `search-btn ${searchType === "brands" ? "brand-btn" : "brand-btn-active"}`;
-  searchProductBtn.className = `search-btn ${searchType === "parts" ? "products-btn" : "products-btn-active"}`;
-  searchWarrantyBtn.className = `search-btn ${searchType === "warranty" ? "warranty-btn" : "warranty-btn-active"}`;
-  searchShopBtn.className = `search-btn ${searchType === "shops" ? "shop-btn" : "shop-btn-active"}`;
+  searchBrandBtn.className = `search-btn ${searchType === "brands" ? "brand-btn-active" : "brand-btn"}`;
+  searchProductBtn.className = `search-btn ${searchType === "parts" ? "products-btn-active" : "products-btn"}`;
+  searchWarrantyBtn.className = `search-btn ${searchType === "warranty" ? "warranty-btn-active" : "warranty-btn"}`;
+  searchShopBtn.className = `search-btn ${searchType === "shops" ? "shop-btn-active" : "shop-btn"}`;
 }
