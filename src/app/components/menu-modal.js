855d51b41976e85868068fe6a51cb121b0399911
Auxcodes <dev@aux.codes>
2024-09-11 11:34:05 +1000

    Move css import from style sheet to index.html

--- src/app/components/menu-modal.js
+++ src/app/components/menu-modal.js
@@ -1,13 +1,14 @@
 class MenuModal extends HTMLElement {
-    set menu(menu) {
-        this.innerHTML = `
+  set menu(menu) {
+    this.innerHTML = `
             <nav id="site-nav" class="site-nav">
                 <ul>
-                    <li id="add-shop-link"><button title="Add new shop"><i class="fa fa-plus site-nav--icon"></i>Add Store</button></li>
+                    <li id="add-shop-link"><button title="Add new shop"><i class="site-nav--icon"></i>Add Store</button></li>
                     <li id="history-link" style="display: none;"><button title="View change history"><i class="fa fa-history site-nav--icon"></i>History</button></li>
-                    <li id="contact-link"><button title="Contact Us"><i class="fa fa-envelope site-nav--icon"></i>Contact</button></li>
-                    <li id="logout-link"><button title="Logout"><i class="fa fa-sign-out site-nav--icon"></i>Logout</button></li>
-                    <li id="login-link"><button title="Login"><i class="fa fa-sign-in site-nav--icon"></i>Login</button></li>
+                    <li id="contact-link"><button title="Contact Us"><i class="site-nav--icon"></i>Contact</button></li>
+                    <li id="logout-link"><button title="Logout"><i class="site-nav--icon"></i>Logout</button></li>
+                    <li id="login-link"><button title="Login"><i class="site-nav--icon"></i>Login</button></li>
+                    <li id="manage-link" class="nav-btn--hide"><button id="browseManageBtn" type="button" title="Manage Site"><i class="site-nav--icon"></i>Manage</button></li>
                 </ul>
             </nav>
             <div id="menu-toggle" class="menu-toggle">
@@ -15,7 +16,7 @@ class MenuModal extends HTMLElement {
             </div>
             <button id="notif-toggle" class="notification-toggle" title="Notifications"><i id="notif-icon" class="fa fa-bell notif-toggle--icon"></i></button>
         `;
-    }
+  }
 }
 
-customElements.define('menu-modal', MenuModal);
+customElements.define("menu-modal", MenuModal);
